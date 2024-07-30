1255a21e3ec31e53e5fa2f6dbc90f21bb27e92c7
Alex Lievense <alexlievense@gmail.com>
2024-07-30 09:07:56 +0200

    feat: add Job Tag pivot table and add corresponding relationships

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    public function jobs(): BelongsToMany
+    {
+        return $this->belongsToMany(Job::class, 'job_listing_tag', 'tag_id', 'job_listing_id');
+    }
 }

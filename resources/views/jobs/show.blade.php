7a12ea2385f2b62d7042ccf781c65649e01f9d1e
Alex Lievense <alexlievense@gmail.com>
2024-08-02 18:32:56 +0200

    refactor: rename Components to components

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,4 +3,8 @@
 
     <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
     <p>This job pays {{ $job['salary'] }} per year.</p>
+
+    <p class="mt-6">
+        <x-button>Edit Job</x-button>
+    </p>
 </x-layout>

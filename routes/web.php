229c20b90998b72eb0f0a6f45653e8e8f8bc3c62
Alex Lievense <alexlievense@gmail.com>
2024-08-04 21:08:02 +0200

    refactor: move Job routes into Route::controller group

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-// Index
-Route::get('/jobs', [JobController::class, 'index']);
-
-// Create
-Route::get('/jobs/create', [JobController::class, 'create']);
-
-// Show
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-
-// Store
-Route::post('/jobs', [JobController::class, 'store']);
-
-// Edit
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-
-// Update
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-
-// Destroy
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
+Route::controller(JobController::class)->group(function(){
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+});
 
 Route::view('/contact', 'contact');

3f86192ac5ea34fa54a76ba860723af5cf9d946a
Alex Lievense <alexlievense@gmail.com>
2024-08-08 21:33:37 +0200

    feat: add register route logic

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,26 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
-    public function create()
-    {
-        return view('auth.register');
-    }
-
     public function store()
     {
-        // validate
-        request()->validate([
+        $validatedAttributes = request()->validate([
             'first_name' => ['required'],
             'last_name' => ['required'],
             'email' => ['required', 'email'],
             'password' => ['required', Password::min(6), 'confirmed'],
         ]);
 
-        // insert
-        // redirect
+        $user = User::create($validatedAttributes);
+
+        Auth::login($user);
+
+        return redirect('/jobs');
+    }
+
+    public function create()
+    {
+        return view('auth.register');
     }
 }
